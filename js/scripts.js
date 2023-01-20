518ed4d61a72f0afffe46300ffc5a9f5ee00dda6
keysme <dnwnrud@gmail.com>
2023-01-19 16:05:13 -0800

    Modify animation
    Modify animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+// Menu toggle --------------------------------------------
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
@@ -7,3 +7,15 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+// Intro Logo animation --------------------------------------------
+var demo = document.getElementById("logoAni");
+var tl = new TimeLineMax({repeat:0});
+var ease = Linear.easeNone;
+
+TweenMax.set(demo, {autoAlpha:1});
+TweenMax.set("path", {drawSVG:0, stroke:"#42a6e0"});
+
+tl.to("#w", 4, {drawSVG:true, ease:ease});
+tl.to("#w1", 3, {drawSVG:true, ease:ease});
+tl.to("#o", 3, {drawSVG:true, ease:ease});
